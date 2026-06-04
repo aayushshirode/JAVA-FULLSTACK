553c7d8e5c6435449970c8a6f8a56535fb8c738e
Aayush Shirode <aayushshirode2007@gamil.com>
2026-06-04 17:26:12 +0530

    Upload project

---
+++ sumoftwo.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[4]={15,7,11,2};
+    int target=9;
+    int n=4;
+
+    for(int i=0; i<=n-1; i++){
+        for(int j=i; j<=n-1; j++){
+            if (arr[i]+arr[j]==target){
+                cout<<"Elements found at: "<<i << "," <<j <<endl;
+                cout<<"Sum = "<< target <<endl;           
+                return 0;
+        }
+    }
+ }
+}
