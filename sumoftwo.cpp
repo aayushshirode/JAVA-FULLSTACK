7ce293856687657268b8b2254798ccd90996d408
Aayush Shirode <aayushshirode2007@gamil.com>
2026-06-04 18:03:12 +0530

    Update sumoftwo.cpp

--- sumoftwo.cpp
+++ sumoftwo.cpp
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 int main(){
     int arr[4]={15,7,11,2};
-    int target=9;
+    int target=17;
     int n=4;
 
     for(int i=0; i<=n-1; i++){
@@ -15,22 +14,4 @@ int main(){
         }
     }
  }
-=======
-#include<iostream>
-using namespace std;
-int main(){
-    int arr[4]={15,7,11,2};
-    int target=9;
-    int n=4;
-
-    for(int i=0; i<=n-1; i++){
-        for(int j=i; j<=n-1; j++){
-            if (arr[i]+arr[j]==target){
-                cout<<"Elements found at: "<<i << "," <<j <<endl;
-                cout<<"Sum = "<< target <<endl;           
-                return 0;
-        }
-    }
- }
->>>>>>> 8f26735132219c47d52863b0d25936b13cf692bc
 }
